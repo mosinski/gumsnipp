d2c61a1b4d1ef47ddaf0e37fd0bace97b4e04171
Miłosz Osiński <mylosz@yahoo.pl>
2014-06-14 17:11:06 +0200

    add nickname to users

--- db/migrate/20140606224048_devise_create_users.rb
+++ db/migrate/20140606224048_devise_create_users.rb
@@ -2,6 +2,7 @@ class DeviseCreateUsers < ActiveRecord::Migration
   def change
     create_table(:users) do |t|
       ## Database authenticatable
+      t.string :nickname,           null: false, default: ""
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
@@ -30,10 +31,10 @@ class DeviseCreateUsers < ActiveRecord::Migration
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
-
       t.timestamps
     end
 
+    add_index :users, :nickname,             unique: true
     add_index :users, :email,                unique: true
     add_index :users, :reset_password_token, unique: true
     # add_index :users, :confirmation_token,   unique: true
--- db/schema.rb
+++ db/schema.rb
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20140613190946) do
   end
 
   create_table "users", force: true do |t|
+    t.string   "nickname",               default: "", null: false
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
@@ -39,6 +40,7 @@ ActiveRecord::Schema.define(version: 20140613190946) do
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["nickname"], name: "index_users_on_nickname", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
   create_table "visit_details", force: true do |t|
