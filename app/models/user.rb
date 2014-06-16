8ac9892b9f7a5a5ebf40a57e509535b0200cc271
Miłosz Osiński <mylosz@yahoo.pl>
2014-06-16 23:00:43 +0200

    correct css & forms errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
   has_many :snipss
 
+  validates :nickname, presence: true
+  validates :nickname, uniqueness: true
+
   def gravatar(size)
     gravatar_id = Digest::MD5.hexdigest(self.email.downcase)
     "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{size}"
