e4ba2c3ec63670c9cfa1e6b7464ddf1a15b8dbdb
Miłosz Osiński <mylosz@yahoo.pl>
2014-06-30 16:02:32 +0200

    finish github omniauth

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,14 +1,12 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def github
-    @user = User.from_omniauth(request.env["omniauth.auth"])
+    user = User.from_omniauth(request.env["omniauth.auth"])
 
-    if @user.persisted?
-      sign_in_and_redirect @user, :event => :authentication, flash: { success: "Successfully Registered" }
+    if user.persisted?
+      sign_in_and_redirect(user)
     else
-      session["devise.github_data"] = request.env["omniauth.auth"]
-      #redirect_to new_user_registration_url
-      #redirect_to edit_user_registration_path, flash: { error: "This file type is not supported. Only files with the following extensions are allowed: jpg jpeg png" }
-      render :template => 'users/new'
+      session["devise.user_attributes"] = user.attributes
+      redirect_to new_user_registration_url
     end
   end
 end
