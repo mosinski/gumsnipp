8e5182820920f85cee669cf5172d04a08719e7d4
Miłosz Osiński <mylosz@yahoo.pl>
2014-07-12 02:28:06 +0200

    add calculating gradient tags

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,13 @@ module ApplicationHelper
   def to_checked
     Snipp.where(published: false, to_check: true).search(params[:search], params[:page]).count
   end
+
+  def lighten_color(hex_color, amount=0.6)
+    hex_color = hex_color.gsub('#','')
+    rgb = hex_color.scan(/../).map {|color| color.hex}
+    rgb[0] = [(rgb[0].to_i + 255 * amount).round, 255].min
+    rgb[1] = [(rgb[1].to_i + 255 * amount).round, 255].min
+    rgb[2] = [(rgb[2].to_i + 255 * amount).round, 255].min
+    "#%02x%02x%02x" % rgb
+  end
 end
